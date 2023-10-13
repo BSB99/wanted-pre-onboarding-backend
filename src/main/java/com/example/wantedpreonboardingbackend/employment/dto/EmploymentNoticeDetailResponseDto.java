dbaf832fce4012410cbf31a19f344ed3160e7928
SeungBum Baek <wer06099@naver.com>
2023-10-14 01:30:22 +0900

    Add(BSB): 기업공고 상세 조회 responseDto 생성#1

---
+++ src/main/java/com/example/wantedpreonboardingbackend/employment/dto/EmploymentNoticeDetailResponseDto.java
@@ -0,0 +1,26 @@
+package com.example.wantedpreonboardingbackend.employment.dto;
+
+import com.example.wantedpreonboardingbackend.employment.EmploymentNotice;
+import lombok.Getter;
+
+@Getter
+public class EmploymentNoticeDetailResponseDto {
+    private Long id;
+    private String company_name;
+    private String country;
+    private String region;
+    private String position;
+    private int money;
+    private String technology;
+    private String description;
+
+    public EmploymentNoticeDetailResponseDto(EmploymentNotice employmentNotice) {
+        this.id = employmentNotice.getId();
+        this.company_name = employmentNotice.getCompany().getName();
+        this.country = employmentNotice.getCompany().getCountry();
+        this.region = employmentNotice.getPosition();
+        this.money = employmentNotice.getMoney();
+        this.technology = employmentNotice.getTechnology();
+        this.description = employmentNotice.getDescription();
+    }
+}
