f9c5ee54ded6148aa247020010bdbd7488a33575
SeungBum Baek <wer06099@naver.com>
2023-10-14 09:42:51 +0900

    Add(BSB): GlobalExceptionHandler 적용 #5

---
+++ src/main/java/com/example/wantedpreonboardingbackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.wantedpreonboardingbackend.exception;
+
+import com.example.wantedpreonboardingbackend.common.ApiResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<ApiResponseDto> handleException(IllegalArgumentException ex) {
+        ApiResponseDto restApiException = new ApiResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                restApiException,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
