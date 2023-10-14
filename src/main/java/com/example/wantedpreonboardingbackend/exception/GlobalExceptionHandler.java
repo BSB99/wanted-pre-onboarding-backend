bec95ebb6db85564327c56fcdea177905b575b54
SeungBum Baek <wer06099@naver.com>
2023-10-14 09:51:23 +0900

    Add(BSB): GlobalExceptionHandler SQLIntegrityConstraintViolationException 추가 #5

--- src/main/java/com/example/wantedpreonboardingbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/wantedpreonboardingbackend/exception/GlobalExceptionHandler.java
@@ -6,10 +6,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler({IllegalArgumentException.class})
     public ResponseEntity<ApiResponseDto> handleException(IllegalArgumentException ex) {
+        ApiResponseDto restApiException = new ApiResponseDto(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(
+                restApiException,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler({SQLIntegrityConstraintViolationException.class})
+    public ResponseEntity<ApiResponseDto> handleException(SQLIntegrityConstraintViolationException ex) {
         ApiResponseDto restApiException = new ApiResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
         return new ResponseEntity<>(
                 restApiException,
