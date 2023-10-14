263c4991e72948ec9f4cc2c74260e86cd3b48b41
SeungBum Baek <wer06099@naver.com>
2023-10-14 09:45:11 +0900

    Add(BSB): Company nullAble false 적용 #2

--- src/main/java/com/example/wantedpreonboardingbackend/company/Company.java
+++ src/main/java/com/example/wantedpreonboardingbackend/company/Company.java
@@ -16,10 +16,13 @@ public class Company {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String country;
 
+    @Column(nullable = false)
     private String region;
 
     public Company(CompanyRequestDto requestDto) {
